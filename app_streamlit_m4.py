2029e8738cd9bbb8034ca79bdd93ff53911c7ba2
Ansh2610 <anshg2603@gmail.com>
2025-11-05 10:16:07 -0700

    bumped to v0.4.2

--- app_streamlit_m4.py
+++ app_streamlit_m4.py
@@ -20,7 +20,7 @@ from backend.flags import (
 )
 
 # ---- Configuration ----
-APP_VERSION = "0.4.1"  # Added explainable match feature
+APP_VERSION = "0.4.2"  # Fixed Streamlit Cloud deployment
 API_BASE_URL = os.getenv("API_BASE_URL", "http://127.0.0.1:8000")
 
 # ---- Logging ----
